aa0a5bbe7b67f6ba68f82d1187568337da02a86e
Ezra Earl Villanueva <ezraearlvillanueva@gmail.com>
2025-03-24 01:17:59 +0800

    Fixed/Added dropdown and avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+import { ChevronDown } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 const Header = () => {
 	const navigate = useNavigate();
@@ -23,15 +31,33 @@ const Header = () => {
     <header className="border-grid sticky top-0 z-50 w-full bg-gray-950">
       <div className='flex flex-row items-center justify-between px-4 xl:px-8 xl:py-6 py-4'>
         <h1 className='lg:text-2xl text-lg font-medium font-playfair text-primary-foreground'>Exam <span className='text-blue-500'>track</span></h1>
-				<div>
-					<a href='mailto:ezraearlvillanueva@gmail.com' className='text-sm text-primary-foreground'>My Account</a>
-					<Button
-						variant="link"
-						className="text-primary-foreground"
-						onClick={handleLogout}
-					>
-						Logout
-					</Button>
+				<div className="flex flex-row">
+					<Avatar>
+						<AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+						<AvatarFallback>CN</AvatarFallback>
+					</Avatar>
+					<DropdownMenu>
+						<DropdownMenuTrigger>
+							<Button
+								variant="link"
+								className="text-primary-foreground"
+							>
+								My Account
+								<ChevronDown />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							<DropdownMenuItem>
+								<Button
+								variant="link"
+								className="text-secondary-foreground no-underline"
+								onClick={handleLogout}
+								>
+									Logout
+								</Button>
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
 				</div>
         
         {/* <h1 className='font-medium lg:text-xl text-zinc-500 hidden xl:block'>Available from March</h1> */}
